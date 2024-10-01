802157817352fb3c8bffc66140500390425574b4
Michal P <159123359+Majkosss@users.noreply.github.com>
2024-10-01 10:23:09 +0000

    hhh

---
+++ main.py
@@ -0,0 +1,16 @@
+pole = [10, 85, 20, 31, 22, 43, 71, 8, 6]
+
+first_hodnota = pole[0]
+middle_hodnota = pole[len(pole)//2]
+last_hodnota = pole[-1]
+
+print("První hodnota:", first_hodnota)
+print("Prostřední hodnota:", middle_hodnota)
+print("Poslední hodnota:", last_hodnota)
+
+pole[4] = 34
+print(pole)
+
+sedma_hodnota = pole[7]
+
+print("7. hodnota:", sedma_hodnota)
